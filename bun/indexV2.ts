578a4da21aba71762fd4ebd59781a6b9e90c22c8
ted <theo_drz@live.com>
2024-06-27 23:37:16 -0400

    implemented async queue, didnt really help much.  Need to find a better way to control RAM usage

--- bun/indexV2.ts
+++ bun/indexV2.ts
@@ -10,34 +10,44 @@ interface Result {
 }
 
 // const bufSize = 2**31;
-const bufSize = 2**28;
+const bufSize = 2**27;
 // const workerCount = 2;
 
 async function run(filePath: string) {
   const file = await open(filePath)
   const chunkCount = Math.ceil((await file.stat()).size / bufSize)
-  const workers: Promise<Result>[] = []
+  // const workers: Promise<Result>[] = []
+  const workers: { [key: string]: Promise<Result> } = {}
+  const results: Result[] = []
   let isDone = 0;
   for (let i = 0; i < chunkCount; i++) {
-    workers.push(
-      new Promise((resolve, reject) => {
-        const worker = new Worker('./workerV2.ts')
-        worker.postMessage({ filePath, i, bufSize })
-        worker.onmessage = (e) => {
-          process.stdout.clearLine(0);
-          process.stdout.cursorTo(0);
-          process.stdout.write(`${++isDone}/${chunkCount}`)
-          resolve(e.data)
-          worker.terminate();
-        }
-        worker.onerror = (err) => {
-          reject(err)
-          throw err
-        }
-      })
-    )
+    // workers.push(
+    if (Object.keys(workers).length > 2) {
+      await Promise.race(
+        // Object.keys(workers).map(workerKey => workers[workerKey])
+        Object.values(workers)
+      )
+    }
+    workers[i.toString()] = new Promise((resolve, reject) => {
+      const worker = new Worker('./workerV2.ts')
+      worker.postMessage({ filePath, i, bufSize })
+      worker.onmessage = (e) => {
+        process.stdout.clearLine(0);
+        process.stdout.cursorTo(0);
+        process.stdout.write(`${++isDone}/${chunkCount}`)
+        delete workers[i.toString()]
+        results[i] = e.data
+        resolve(e.data)
+        worker.terminate();
+      }
+      worker.onerror = (err) => {
+        reject(err)
+        throw err
+      }
+    })
+    // )
   }
-  await Promise.all(workers).then(arr => {
+  await Promise.all(Object.values(workers)).then(arr => {
     // console.log(arr)
   })
 }
