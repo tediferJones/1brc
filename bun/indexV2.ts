d37da5c08ac9dab028049be733e41fe6b359639c
ted <theo_drz@live.com>
2024-06-30 23:34:24 -0400

    verified new version passes all tests, did some clean up and got best time down to sub 100 seconds

--- bun/indexV2.ts
+++ bun/indexV2.ts
@@ -1,29 +1,18 @@
 import { open } from 'fs/promises';
 
-interface Record {
-  min: number,
-  max: number,
-  count: number,
-  total: number,
-}
-
 interface Result {
-  [key: string]: Record 
+  [key: string]: {
+    min: number,
+    max: number,
+    count: number,
+    total: number,
+  }
 }
 
 interface MiniResult {
   first: Buffer,
   last: Buffer,
   result: Result,
-}
-
-interface SomeWorker {
-  worker: Worker,
-  promiseInfo?: {
-    promise: Promise<MiniResult>,
-    resolve: Function,
-    reject: Function,
-  }
 }
 
 function processLine(line: string, result: Result) {
@@ -55,73 +44,82 @@ function format(result: Result) {
 
 // const bufSize = 2**31;
 const bufSize = 2**24;
-// const workerCount = 2;
+const workerCount = 8;
 
-async function run(filePath: string) {
+class MyWorker {
+  worker: Worker;
+  promiseInfo?: {
+    promise: Promise<MiniResult>,
+    resolve: Function,
+    reject: Function,
+  };
+  finalResult: Result;
+
+  constructor(results: MiniResult[], finalResult: Result) {
+    const worker = new Worker('./workerV2.ts')
+    worker.onmessage = (e) => {
+      // process.stdout.clearLine(0);
+      // process.stdout.cursorTo(0);
+      // process.stdout.write(`${++isDone}`)
+      // results[e.data.i] = e.data
+      mergeResults(e.data.result, finalResult)
+      delete e.data.result
+      results[e.data.i] = e.data
+      this.promiseInfo?.resolve()
+      this.promiseInfo = undefined;
+    }
+    worker.onerror = (err) => { throw err }
+    this.worker = worker
+    this.finalResult = finalResult
+  }
+
+  sendMsg(msgObj: { filePath: string, i: number, bufSize: number }) {
+    if (this.promiseInfo) throw Error('promise already exists')
+    this.worker.postMessage(msgObj)
+    let resolve, reject;
+    const promise = new Promise<MiniResult>((res, rej) => {
+      [ resolve, reject ] = [ res, rej ];
+    })
+    if (!resolve || !reject) throw Error('cant find resolve and reject')
+    this.promiseInfo = { promise, resolve, reject }
+  }
+}
+
+function mergeResults(miniResult: Result, results: Result) {
+  // const prev = Buffer.from(miniResult.last || '')
+  // const current = Buffer.from(miniResult.first)
+  // const tempBuffer = Buffer.alloc((prev.length) + current.length);
+  // prev.copy(tempBuffer)
+  // current.copy(tempBuffer, prev.length)
+  // processLine(tempBuffer.toString('utf8', 0, tempBuffer.length - 1), results)
+
+  Object.keys(miniResult).forEach(key => {
+    const current = miniResult;
+    if (results[key]) {
+      if (current[key].min < results[key].min) {
+        results[key].min = current[key].min
+      }
+      if (current[key].max > results[key].max) {
+        results[key].max = current[key].max
+      }
+      results[key].count += current[key].count
+      results[key].total += current[key].total
+    } else {
+      results[key] = current[key]
+    }
+  })
+}
+
+let isDone = 0;
+export async function runV2(filePath: string) {
   const file = await open(filePath)
   const chunkCount = Math.ceil((await file.stat()).size / bufSize)
-  const workers: { [key: string]: Promise<MiniResult> } = {}
   const results: MiniResult[] = []
   const finalResult: Result = {};
-  let isDone = 0;
 
-  // for (let i = 0; i < chunkCount; i++) {
-  //   // if (i > 4) break;
-  //   if (Object.keys(workers).length > 8) await Promise.race(Object.values(workers))
-  //   workers[i.toString()] = new Promise((resolve, reject) => {
-  //     const worker = new Worker('./workerV2.ts')
-  //     worker.postMessage({ filePath, i, bufSize })
-  //     worker.onmessage = (e) => {
-  //       process.stdout.clearLine(0);
-  //       process.stdout.cursorTo(0);
-  //       process.stdout.write(`${++isDone}/${chunkCount}`)
-  //       delete workers[i.toString()]
-  //       results[i] = e.data
-  //       resolve(e.data)
-  //       worker.terminate();
-  //     }
-  //     worker.onerror = (err) => {
-  //       reject(err)
-  //       throw err
-  //     }
-  //   })
-  // }
-  // await Promise.all(Object.values(workers))
-
-  class MyWorker {
-    worker: Worker;
-    promiseInfo?: {
-      promise: Promise<MiniResult>,
-      resolve: Function,
-      reject: Function,
-    }
-
-    constructor() {
-      const worker = new Worker('./workerV2.ts')
-      worker.onmessage = (e) => {
-        process.stdout.clearLine(0);
-        process.stdout.cursorTo(0);
-        process.stdout.write(`${++isDone}/${chunkCount}`)
-        results[e.data.i] = e.data
-        this.promiseInfo?.resolve()
-        this.promiseInfo = undefined;
-      }
-      worker.onerror = (err) => { throw err }
-      this.worker = worker
-    }
-
-    sendMsg(i: number) {
-      if (this.promiseInfo) throw Error('promise already exists')
-      this.worker.postMessage({ filePath, i, bufSize })
-      let resolve, reject;
-      const promise = new Promise<MiniResult>((res, rej) => {
-        [ resolve, reject ] = [ res, rej ];
-      })
-      if (!resolve || !reject) throw Error('cant find resolve and reject')
-      this.promiseInfo = { promise, resolve, reject }
-    }
-  }
-  const myWorkers: MyWorker[] = [...Array(8).keys()].map(() => new MyWorker())
+  const myWorkers: MyWorker[] = [
+    ...Array(workerCount).keys()
+  ].map(() => new MyWorker(results, finalResult))
 
   for (let i = 0; i < chunkCount; i++) {
     let worker = myWorkers.find(worker => !worker.promiseInfo);
@@ -130,45 +128,79 @@ async function run(filePath: string) {
       worker = myWorkers.find(worker => !worker.promiseInfo);
     }
     if (!worker) throw Error('no worker')
-    worker.sendMsg(i)
+    worker.sendMsg({ filePath, i, bufSize })
   }
 
   await Promise.all(myWorkers.map(worker => worker.promiseInfo?.promise));
   myWorkers.forEach(worker => worker.worker.terminate());
 
-  console.log('')
   for (let i = 0; i < results.length; i++) {
-    const prev = Buffer.from(results[i - 1]?.last || '')
-    const current = Buffer.from(results[i].first)
-    const tempBuffer = Buffer.alloc((prev.length) + current.length);
-    prev.copy(tempBuffer)
-    current.copy(tempBuffer, prev.length)
-    processLine(tempBuffer.toString('utf8', 0, tempBuffer.length - 1), finalResult)
+    // const prev = Buffer.from(results[i - 1]?.last || '')
+    // const current = Buffer.from(results[i].first)
+    // const tempBuffer = Buffer.alloc((prev.length) + current.length);
+    // prev.copy(tempBuffer)
+    // current.copy(tempBuffer, prev.length)
+    // processLine(tempBuffer.toString('utf8', 0, tempBuffer.length - 1), finalResult)
+    // Object.keys(results[i].result).forEach(key => {
+    //   const current = results[i].result;
+    //   if (finalResult[key]) {
+    //     if (current[key].min < finalResult[key].min) {
+    //       finalResult[key].min = current[key].min
+    //     }
+    //     if (current[key].max > finalResult[key].max) {
+    //       finalResult[key].max = current[key].max
+    //     }
+    //     finalResult[key].count += current[key].count
+    //     finalResult[key].total += current[key].total
+    //   } else {
+    //     finalResult[key] = current[key]
+    //   }
+    // })
 
-    Object.keys(results[i].result).forEach(key => {
-      const current = results[i].result;
-      if (finalResult[key]) {
-        if (current[key].min < finalResult[key].min) {
-          finalResult[key].min = current[key].min
-        }
-        if (current[key].max > finalResult[key].max) {
-          finalResult[key].max = current[key].max
-        }
-        finalResult[key].count += current[key].count
-        finalResult[key].total += current[key].total
-      } else {
-        finalResult[key] = current[key]
-      }
-    })
+    processLine(
+      Buffer.concat([
+        results[i - 1]?.last || new Uint8Array(),
+        results[i].first
+      ]).toString('utf8'),
+      finalResult,
+    )
+    // mergeResults(results[i].result, finalResult)
   }
-  console.log('done')
   return format(finalResult)
 }
 
 const start = Bun.nanoseconds();
-const result = await run('../measurements.txt');
+const result = await runV2('../measurements.txt');
 const end = Bun.nanoseconds();
-// console.log(result)
 console.log(`${(end - start) / 10**9}`)
 console.log(`${result}\n` === (await Bun.file('../notes/answer.txt').text()))
-// Best time: 112.5 seconds, 8 workers, 2*24 bufSize
+
+// Best times:
+// 97.8 seconds, 8 workers, 2**24 bufSize
+//  - This score was achieved by merging all results after workers are done
+// 109.8 seconds, 8 workers, 2**24 bufSize
+// 111.1 seconds, 8 workers, 2**24 bufSize
+// 112.5 seconds, 8 workers, 2**24 bufSize
+
+// OLD
+// for (let i = 0; i < chunkCount; i++) {
+//   if (Object.keys(workers).length > 8) await Promise.race(Object.values(workers))
+//   workers[i.toString()] = new Promise((resolve, reject) => {
+//     const worker = new Worker('./workerV2.ts')
+//     worker.postMessage({ filePath, i, bufSize })
+//     worker.onmessage = (e) => {
+//       process.stdout.clearLine(0);
+//       process.stdout.cursorTo(0);
+//       process.stdout.write(`${++isDone}/${chunkCount}`)
+//       delete workers[i.toString()]
+//       results[i] = e.data
+//       resolve(e.data)
+//       worker.terminate();
+//     }
+//     worker.onerror = (err) => {
+//       reject(err)
+//       throw err
+//     }
+//   })
+// }
+// await Promise.all(Object.values(workers))
--- bun/samples.test.ts
+++ bun/samples.test.ts
@@ -1,13 +1,15 @@
 import { readdir } from "node:fs/promises";
 import { expect, test } from "bun:test";
 import { run, easyMode } from "./index";
+import { runV2 } from "./indexV2";
 
 const dir = await readdir('samples/')
 dir.filter(file => file.match(/\.txt$/)).toSorted().forEach(inputFile => {
   console.log(inputFile)
   test(inputFile, async () => {
     // const myResult = await run(`./samples/${inputFile}`)
-    const myResult = await easyMode(`./samples/${inputFile}`)
+    // const myResult = await easyMode(`./samples/${inputFile}`)
+    const myResult = await runV2(`./samples/${inputFile}`)
     const testResult = await Bun.file(
         `./samples/${inputFile.slice(0, inputFile.lastIndexOf('.')) + '.out'}`
       ).text()
