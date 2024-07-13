ced1a6ef433d9f6886cab93049dda9f9e2cb76d7
ted <theo_drz@live.com>
2024-07-12 23:36:34 -0400

    new version works synchronously

--- go/newSync/main.go
+++ go/newSync/main.go
@@ -5,6 +5,8 @@ import (
 	"math"
 	"os"
 	"sort"
+	"strconv"
+	"strings"
 	"time"
 )
 
@@ -19,6 +21,16 @@ func check(e error) {
   if e != nil {
     panic(e)
   }
+}
+
+func findLast(findVal byte, bytes []byte) int {
+  for i := range bytes {
+    index := len(bytes) - i - 1
+    if bytes[index] == findVal {
+      return index
+    }
+  }
+  return -1
 }
 
 func getSortedKeys(result map[string]Record) []string {
@@ -48,14 +60,14 @@ func prettyPrint(result map[string]Record) string {
   return finalString[:len(finalString) - 2] + "}\n"
 }
 
-func processChunk(
-  chunkNum int,
-  filePath string,
-  startIndex *int,
-  result *map[string]Record,
-) {
-  // miniResult := map[string]Record{}
-}
+// func processChunk(
+//   chunkNum int,
+//   filePath string,
+//   startIndex *int,
+//   result *map[string]Record,
+// ) {
+//   // miniResult := map[string]Record{}
+// }
 
 func main() {
   start := time.Now()
@@ -68,7 +80,7 @@ func main() {
   fileInfo, err := file.Stat()
   check(err)
   size := fileInfo.Size()
-  chunkCount := int(math.Ceil(float64(size) / float64(bufSize)))
+  // chunkCount := int(math.Ceil(float64(size) / float64(bufSize)))
   startIndex := 0
   result := map[string]Record{}
 
@@ -76,8 +88,56 @@ func main() {
   // Then set the index of \n as the start value for the next chunk
   // This way we dont need worry about handling partial lines
 
-  for i := 0; i < chunkCount; i++ {
-    processChunk(i, filePath, &startIndex, &result)
+  // for i := 0; i < chunkCount; i++ {
+  //   processChunk(i, filePath, &startIndex, &result)
+  // }
+
+  for startIndex < int(size) {
+    myBuffer := make([]byte, bufSize)
+    file.Seek(int64(startIndex), 0)
+    readCount, err := file.Read(myBuffer)
+    if (err != nil) {
+      check(err)
+    }
+    if (readCount < int(bufSize)) {
+      fmt.Println("end of file")
+    }
+
+    lastNewLine := findLast(10, myBuffer)
+
+    startIndex = startIndex + lastNewLine + 1
+    percent := fmt.Sprintf("%.2f%%", (float64(startIndex) / float64(size)) * 100)
+    fmt.Println(
+      "starting from",
+      startIndex,
+      percent,
+      )
+
+    split := strings.Split(string(myBuffer[:lastNewLine]), "\n")
+    for _, line := range split {
+      splitLine := strings.Split(line, ";")
+      item, ok := (result)[splitLine[0]]
+      num, err := strconv.ParseFloat(splitLine[1], 64)
+      check(err)
+      if ok {
+        if item.min > num {
+          item.min = num
+        }
+        if item.max < num {
+          item.max = num
+        }
+        item.total += num
+        item.count += 1
+      } else {
+        item = Record{
+          min: num,
+          max: num,
+          total: num,
+          count: 1,
+        }
+      }
+      (result)[splitLine[0]] = item
+    }
   }
 
   myAnswer := prettyPrint(result)
