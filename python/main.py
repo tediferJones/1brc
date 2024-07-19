6b14f82808a052c3bb8069c3ec2312b54984f172
ted <theo_drz@live.com>
2024-07-18 23:40:18 -0400

    got python version fully working synchronously, but it is amazingly slow

--- python/main.py
+++ python/main.py
@@ -1,5 +1,6 @@
 from typing import Dict, List, TypedDict
 import time
+import difflib
 
 class Record(TypedDict):
     min: float
@@ -16,7 +17,14 @@ class PartialResult(TypedDict):
 
 def mergeResults(partialResults: List[PartialResult]):
     finalResult: Result = {}
-    for partialResult in partialResults:
+    for i, partialResult in enumerate(partialResults):
+        if i > 0:
+            line = partialResults[i - 1]['last'] + partialResult['first']
+        else:
+            line = partialResult['first']
+        print('assembled line', line.decode('utf-8'))
+        processLine(line.decode('utf-8'), finalResult)
+
         # We need to handle partial lines some where around here
         miniResult = partialResult['miniResult']
         for city in miniResult:
@@ -37,7 +45,31 @@ def mergeResults(partialResults: List[PartialResult]):
     return finalResult
 
 def prettyPrint(result: Result):
-    return ''
+    finalString = '{'
+    for key in sorted(result.keys()):
+        average = result[key]['total'] / result[key]['count']
+        finalString += f"{key}={round(result[key]['min'], 1)}/{round(average, 1)}/{round(result[key]['max'], 1)}, "
+
+    return finalString[:len(finalString) - 2] + '}\n'
+
+def processLine(line: str, miniResult: Result):
+    city, num = line.split(';')
+    temp = float(num)
+
+    if city in miniResult:
+        if miniResult[city]['min'] > temp:
+            miniResult[city]['min'] = temp
+        if miniResult[city]['max'] < temp:
+            miniResult[city]['max'] = temp
+        miniResult[city]['total'] += temp
+        miniResult[city]['count'] += 1
+    else:
+        miniResult[city] = {
+            'min': temp,
+            'max': temp,
+            'total': temp,
+            'count': 1,
+        }
 
 def main():
     print('Main func');
@@ -54,6 +86,8 @@ def main():
 
         count += 1
         print(count)
+        # if count > 4:
+        #     break
 
         endOfFirstLine = chunk.find(10) + 1
         startOfLastLine = chunk.rfind(10) + 1
@@ -69,33 +103,50 @@ def main():
 
             line = mainChunk[startIndex:endOfNextLine].decode('utf-8')
             startIndex = endOfNextLine + 1
-            city, num = line.split(';')
-            temp = float(num)
+            processLine(line, miniResult)
+            # city, num = line.split(';')
+            # temp = float(num)
 
-            if city in miniResult:
-                if miniResult[city]['min'] > temp:
-                    miniResult[city]['min'] = temp
-                if miniResult[city]['max'] < temp:
-                    miniResult[city]['max'] = temp
-                miniResult[city]['total'] += temp
-                miniResult[city]['count'] += 1
-            else:
-                miniResult[city] = {
-                    'min': temp,
-                    'max': temp,
-                    'total': temp,
-                    'count': 1,
-                }
+            # if city in miniResult:
+            #     if miniResult[city]['min'] > temp:
+            #         miniResult[city]['min'] = temp
+            #     if miniResult[city]['max'] < temp:
+            #         miniResult[city]['max'] = temp
+            #     miniResult[city]['total'] += temp
+            #     miniResult[city]['count'] += 1
+            # else:
+            #     miniResult[city] = {
+            #         'min': temp,
+            #         'max': temp,
+            #         'total': temp,
+            #         'count': 1,
+            #     }
 
         miniResults.append({
             'first': chunk[:endOfFirstLine],
             'last': chunk[startOfLastLine:],
             'miniResult': miniResult,
         })
-        print('Chunk Time:', time.time() - start)
+        # print('Chunk Time:', time.time() - start)
 
     finalResult = mergeResults(miniResults)
-    prettyPrint(finalResult)
+    finalString = prettyPrint(finalResult)
     print('Run Time:', time.time() - start)
+
+    with open('../notes/answer.txt') as file:
+        answer = file.read()
+        # print('THIS IS THE ANSWER\n', answer)
+        # print('THIS IS THE RESULT\n', finalString)
+
+        # # Show diff between result and answer
+        # diff = difflib.ndiff(answer, finalString)
+        # diff_list = list(diff)
+
+        # # Print only the differences
+        # for i, s in enumerate(diff_list):
+        #     if s[0] in ('-', '+'):
+        #         print(f"Line {i}: {s}")
+
+        print(finalString + '\n' == answer)
 
 main();
